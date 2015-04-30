57cb1b550a1f58e53ebde0c69d2c43c7f6a05656
linxihui <xihuil.silence@gmail.com>
2015-04-30 09:46:15 -0400

    Added formula format methods and doc

--- R/GaussianProcess.R
+++ R/GaussianProcess.R
@@ -1,11 +1,37 @@
-shogunGP <- function(
+#' @title Fit Gaussian process regression 
+#' @description 
+#' Fit Gaussian process regression using shogun's \code{FITCInferenceMethod} and \code{ExactInferenceMethod}
+#' @param formula: An R formula
+#' @param data: An data frame
+#' @param x: A design matrix
+#' @param y: A response vector
+#' @param m: Dimension of the low rank approach to the covariance matrix: V_nxn = W_nxm W'_mxn + delta*I_nxn
+#' @param kernel: Only RBF kernel available
+#' @param kpar: A list of kernel parameters
+#' @param var: Gaussian noise for regression
+shogunGP <- function(x, ...) UseMethod('shogunGP');
+
+shogunGP.formula <- function(formula, data, ...) {
+	mf <- model.frame(formula, data);
+	x <- model.matrix(formula, data);
+	y <- model.response(mf);
+	xint <- match("(Intercept)", colnames(x), nomatch = 0);
+	if (xint > 0) x <- x[, -xint, drop = FALSE];
+	rm(mf);
+	out <- shogunGP.default(x, y, ...);
+	out$formula <- formula;
+	class(out) <- c('shogounGP.formula', class(out));
+	return(out);
+	}
+
+shogunGP.default <- function(
 	x, y, m = NULL,
 	kernel ='rbfdot', kpar = list(sigma = 'automatic'), 
 	var = 1
 	) {
 
 	require(shogun);
-	requireNamespace(kernlab);
+	requireNamespace('kernlab');
 	sigma <- ifelse('automatic' == kpar$sigma, kernlab::sigest(x)[[2]], kpar$sigest);
 
 	x <- scale(x);
@@ -37,7 +63,14 @@ shogunGP <- function(
 		);
 	}
 
-predict.shogunGP <- function(object, newdata) {
+predict.shogunGP.formula <- function(object, newdata, ...) {
+	newdata <- model.matrix(object$formula[-2], newdata);
+	xint <- match("(Intercept)", colnames(newdata), nomatch = 0);
+	if (xint > 0) newdata <- newdata[, -xint, drop = FALSE];
+	return(predict.shogunGP(object, newdata));
+	}
+
+predict.shogunGP <- function(object, newdata, ...) {
 	require(shogun);
 	newdata <- scale(newdata, center = object$standardize$x[['scaled:center']], scale = object$standardize$x[['scaled:scale']]);
 	feats_test <- RealFeatures();
@@ -45,11 +78,3 @@ predict.shogunGP <- function(object, newdata) {
 	out <- object$fit$apply(feats_test)$get_labels();
 	return(out*object$standardize$y[['scaled:scale']] + object$standardize$y[['scaled:center']]);
 	}
-
-
-
-dat <- read.csv('Documents/olfaction_intensity.csv');
-dat <- as.matrix(dat);
-
-fit <- shogunGP(dat[, -1], dat[, 1], m = 10);
-pred <- predict(fit, newdata = dat[, -1]);
