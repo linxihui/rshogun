7a8c289f934b06be3b72ed8ed37c3363d4852194
linxihui <xihuil.silence@gmail.com>
2015-05-01 08:41:57 -0400

    added post variance estimate; code clean up

--- R/GaussianProcess.R
+++ R/GaussianProcess.R
@@ -1,14 +1,15 @@
 #' @title Fit Gaussian process regression 
 #' @description 
 #' Fit Gaussian process regression using shogun's \code{FITCInferenceMethod} and \code{ExactInferenceMethod}
-#' @param formula: An R formula
-#' @param data: An data frame
-#' @param x: A design matrix
-#' @param y: A response vector
-#' @param m: Dimension of the low rank approach to the covariance matrix: V_nxn = W_nxm W'_mxn + delta*I_nxn
-#' @param kernel: Only RBF kernel available
-#' @param kpar: A list of kernel parameters
-#' @param var: Gaussian noise for regression
+#' @param formula : An R formula
+#' @param data    : An data frame
+#' @param x       : A design matrix
+#' @param y       : A response vector
+#' @param m       : Dimension of the low rank approach to the covariance matrix : V_nxn = W_nxm W'_mxn + delta*I_nxn
+#' @param kernel  : Only RBF kernel available
+#' @param kpar    : A list of kernel parameters
+#' @param var     : Gaussian noise for regression
+#' @export
 shogunGP <- function(x, ...) UseMethod('shogunGP');
 
 shogunGP.formula <- function(formula, data, ...) {
@@ -25,7 +26,7 @@ shogunGP.formula <- function(formula, data, ...) {
 	}
 
 shogunGP.default <- function(
-	x, y, m = NULL,
+	x, y, m = NULL, scaled = TRUE,
 	kernel ='rbfdot', kpar = list(sigma = 'automatic'), 
 	var = 1
 	) {
@@ -34,30 +35,37 @@ shogunGP.default <- function(
 	requireNamespace('kernlab');
 	sigma <- ifelse('automatic' == kpar$sigma, kernlab::sigest(x)[[2]], kpar$sigest);
 
-	x <- scale(x);
-	y <- scale(y);
+	standardize <- NULL;
+	if (scaled) {
+		x <- scale(x);
+		y <- scale(y);
+		standardize <- list(
+			x = attributes(x)[c('scaled:center', 'scaled:scale')], 
+			y = attributes(y)[c('scaled:center', 'scaled:scale')]
+			);
+		}
+
+	x <- t(x);
 	feats <- RealFeatures();
-	dump <- feats$set_feature_matrix(t(x));
+	dump <- feats$set_feature_matrix(x);
 	labels <- RegressionLabels();
 	dump <- labels$set_labels(y);
 
+	start <- Sys.time();
 	if (!is.null(m)) {
 		feats_inducing <- RealFeatures();
-		dump <- feats_inducing$set_feature_matrix(t(x[sample(nrow(x), size = m), ]));
-
+		dump <- feats_inducing$set_feature_matrix(x[, sample(ncol(x), size = m)]);
 		inf <- FITCInferenceMethod(GaussianKernel(10, 1/sigma), feats, ZeroMean(), labels, GaussianLikelihood(var), feats_inducing);
 	} else {
 		inf <- ExactInferenceMethod(GaussianKernel(10, 1/sigma), feats, ZeroMean(), labels, GaussianLikelihood(var));
 		}
-			
-	start <- Sys.time();
 	gp <- GaussianProcessRegression(inf);
 	gp$train();
-	print(Sys.time() - start);
+	cat('Training time:', Sys.time() - start, '\n');
 
 	return(
 		structure(
-			list(fit = gp, standardize = list(x = attributes(x)[c('scaled:center', 'scaled:scale')], y = attributes(y)[c('scaled:center', 'scaled:scale')])),
+			list(fit = gp, standardize = standardize),
 			class = 'shogunGP'
 			)
 		);
@@ -70,11 +78,17 @@ predict.shogunGP.formula <- function(object, newdata, ...) {
 	return(predict.shogunGP(object, newdata));
 	}
 
-predict.shogunGP <- function(object, newdata, ...) {
+predict.shogunGP <- function(object, newdata, type = c('response', 'raw'), ...) {
 	require(shogun);
+	type <- match.arg(type);
 	newdata <- scale(newdata, center = object$standardize$x[['scaled:center']], scale = object$standardize$x[['scaled:scale']]);
 	feats_test <- RealFeatures();
 	dump <- feats_test$set_feature_matrix(t(newdata));
 	out <- object$fit$apply(feats_test)$get_labels();
-	return(out*object$standardize$y[['scaled:scale']] + object$standardize$y[['scaled:center']]);
+	out <- out*object$standardize$y[['scaled:scale']] + object$standardize$y[['scaled:center']];
+	if ('raw' == type) {
+		var <-object$fit$get_variance_vector(feats_test);
+		out <- cbind(mean = out, stderr = sqrt(var)*object$standardize$y[['scaled:scale']]);
+		}
+	return(out);
 	}
